29beccbd581cdfc30a8672494d8719d1bfcdc7bd
Wojciech Szczepański <wojtek.szczepanski12@gmail.com>
2023-06-22 16:54:10 +0200

    Add registration form and authentication context

---
+++ health-keeper/src/AuthContext/AuthContext.tsx
@@ -0,0 +1,57 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  UserCredential,
+  User,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '../api/firebase/firebase';
+import React, { useContext, useEffect, useState } from 'react';
+
+const AuthContext = React.createContext<AuthContextProps | undefined>(
+  undefined
+);
+
+const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+type AuthContextProps = {
+  currentUser: User | null;
+  register: (email: string, password: string) => Promise<UserCredential>;
+  login: (email: string, password: string) => Promise<UserCredential>;
+};
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children
+}) => {
+
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  // registration
+  function register(email: string, password: string): Promise<UserCredential> {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+//   login
+function login(email:string, password:string): Promise<UserCredential> {
+    return signInWithEmailAndPassword(auth,email,password)
+}
+// listen for auth state change
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+        console.log(user)
+      setCurrentUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value: AuthContextProps = {
+    currentUser,
+    register,
+    login,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export default useAuth;
--- health-keeper/src/api/firebase/firebase.ts
+++ health-keeper/src/api/firebase/firebase.ts
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { firebaseConfig } from "./firebase.config";
+import { getAuth } from "firebase/auth";
 
 export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app)
 
 //https://console.firebase.google.com/u/0/project/jfdzr11cb/overview
