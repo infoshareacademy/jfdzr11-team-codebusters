5b8967c8eb38c2c6460aa6f5570127201f4205df
Julian Filip <jfilip84@gmail.com>
2023-07-14 20:34:34 +0200

    add delMed button, css styling

--- health-keeper/src/components/Medicine/MyMedicine/MyMedicine.tsx
+++ health-keeper/src/components/Medicine/MyMedicine/MyMedicine.tsx
@@ -12,21 +12,25 @@ const MyMedicine = () => {
     setMedicines(userData.medicines);
   }, []);
 
+  const deleteMed = async () => {
+    await console.log('Usuwam lek');
+  };
+
   return (
     <div className={styles.container}>
       <h1>Moje leki</h1>
-      <ul>
-        {medicines.map((medicine) => (
-          <>
-            <h2>{medicine.name}</h2>
-            <p>{medicine.form}</p>
-            <p>{medicine.substance}</p>
-            <p>{medicine.power}</p>
-            <p>{medicine.pack}</p>
-            <p>{medicine.registryNumber}</p>
-          </>
-        ))}
-      </ul>
+
+      {medicines.map((medicine) => (
+        <>
+          <h2>{medicine.name}</h2>
+          <p>{medicine.form}</p>
+          <p>{medicine.substance}</p>
+          <p>{medicine.power}</p>
+          <p>{medicine.pack}</p>
+          <p>{medicine.registryNumber}</p>
+          <button onClick={deleteMed}>Usuń lek</button>
+        </>
+      ))}
     </div>
   );
 };
